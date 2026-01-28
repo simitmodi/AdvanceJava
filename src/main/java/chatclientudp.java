f6f71d7839d8776ed283e07af67c1e8cb02a2eab
BANSI <bansarimakwana3007@gmail.com>
2026-01-28 09:52:41 +0530

    28/01/2026 AJP Lab work

---
+++ src/main/java/chatclientudp.java
@@ -0,0 +1,32 @@
+import javax.xml.crypto.Data;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class chatclientudp {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket socket = new DatagramSocket();
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            //Send Data to server
+            System.out.printf("Client:");
+            String message = sc.nextLine();
+            if(message.equalsIgnoreCase("exit")){
+                socket.close();
+            } else{
+                byte[] sendData = message.getBytes();
+                InetAddress ip = InetAddress.getByName("localhost");
+                DatagramPacket dpSend = new DatagramPacket(sendData, sendData.length, ip, 6969);
+                socket.send(dpSend);
+            }
+
+            //Receive Data form server
+
+            byte[] receiveData = new byte[1024];
+            DatagramPacket dpReceive = new DatagramPacket(receiveData, receiveData.length);
+            socket.receive(dpReceive);
+            String receiveMessage = new String(dpReceive.getData(), 0, dpReceive.getLength());
+            System.out.printf("Received message: %s\n", receiveMessage);
+        }
+    }
+}
---
+++ src/main/java/chatserverudp.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class chatserverudp {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket socket = new DatagramSocket(6969);
+        byte[] buf = new byte[1024];
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+            String message = new String(packet.getData(), 0, packet.getLength());
+            System.out.println("Client: "+message);
+
+            System.out.print("Server: ");
+            String sendMsg = sc.nextLine();
+            if(sendMsg.equalsIgnoreCase("exit")){
+                socket.close();
+            } else {
+                byte[] buf2 = sendMsg.getBytes();
+                InetAddress address = packet.getAddress();
+                int port = packet.getPort();
+                DatagramPacket sendPacket = new DatagramPacket(buf2, buf2.length, address, port);
+                socket.send(sendPacket);
+            }
+        }
+    }
+}
